57382c472e08c7b144167c5879d537201b1ce3bb
Александр <sasaserbakov812@gmail.com>
2023-04-22 09:14:30 +0500

    base moving

---
+++ ShipsBattle/Content/Entity.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShipsBattle.Content
+{
+    public struct Entity
+    {
+        public static int x;
+        public static int y;
+
+        public Entity() 
+        { 
+            x = 0;
+            y = 0;
+        }
+        public void MoveTo(Point directoin)
+        {
+            x += directoin.X;
+            y += directoin.Y;
+        }
+        public Vector2 Position()
+        {
+            return new Vector2(x, y);
+        }
+    }
+}
--- ShipsBattle/Game1.cs
+++ ShipsBattle/Game1.cs
@@ -1,6 +1,8 @@
-﻿using Microsoft.Xna.Framework;
+﻿using Microsoft.VisualBasic;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using ShipsBattle.Content;
 
 namespace ShipsBattle
 {
@@ -8,6 +10,10 @@ namespace ShipsBattle
     {
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
+        Texture2D texture;
+        Vector2 position = new Vector2(10, 10);
+        float speed = 2f;
+        Entity Player = new Entity();
 
         public Game1()
         {
@@ -17,17 +23,14 @@ namespace ShipsBattle
         }
 
         protected override void Initialize()
-        {
-            // TODO: Add your initialization logic here
-
+        {           
             base.Initialize();
         }
 
         protected override void LoadContent()
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            texture = Content.Load<Texture2D>("pirat_ship");
         }
 
         protected override void Update(GameTime gameTime)
@@ -35,7 +38,22 @@ namespace ShipsBattle
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
                 Exit();
 
-            // TODO: Add your update logic here
+            if (Keyboard.GetState().IsKeyDown(Keys.W))
+                Player.MoveTo(Direction.Forward);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.A))
+                Player.MoveTo(Direction.Left);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.S))
+                Player.MoveTo(Direction.Backward);
+
+            if (Keyboard.GetState().IsKeyDown(Keys.D))
+                Player.MoveTo(Direction.Right);
+
+
+            position.X += speed;
+            if (position.X > Window.ClientBounds.Width - texture.Width || position.X < 0)
+                speed *= -1.01f;
 
             base.Update(gameTime);
         }
@@ -44,7 +62,11 @@ namespace ShipsBattle
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
+            _spriteBatch.Begin();
+            _spriteBatch.Draw(texture, position,new Rectangle(100, 0, 120, 200), Color.White);
+
+            _spriteBatch.Draw(texture, Player.Position(), new Rectangle(300, 0, 120, 200), Color.White);
+            _spriteBatch.End();
 
             base.Draw(gameTime);
         }
